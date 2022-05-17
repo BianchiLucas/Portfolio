b6122893075328d95d9a833a5d1800c48060653c
BianchiLucas <lucasbianchi92@gmail.com>
2022-05-16 21:12:33 -0300

    Skills and works update

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,19 +4,25 @@ import javascript from '../assets/javascript.png'
 import react from '../assets/react.png'
 import github from '../assets/github.png'
 import typescript from '../assets/Typescript.png'
+import nextjs from '../assets/nextjs.png'
+import tailwdindcss from '../assets/tailwind.png'
 
 const Skills = () => {
     return (
-        <div id='skills' className='w-full h-screen snap-center'>
-            <div className='max-w-4xl w-full mx-auto my-6 p-4 flex flex-col justify-center h-full'>
+        <div id='skills' className='w-full h-screen snap-center mb-12'>
+            <div className='max-w-7xl w-full mx-auto my-6 p-4 flex flex-col justify-center h-full'>
                 <div className='my-8'>
                     <p className='my-4 font-bold text-4xl inline border-b-4 border-celeste'>Skills</p>
                     <p className='py-4 text-lg'>&gt;_  These are the technologies I've worked whit</p>
                 </div>
-                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center mx-auto'>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center mx-auto'>
                     <div className='bg-blue3/05 shadow-md shadow-bgwindow rounded-md hover:scale-110 duration-300 hover:shadow-celeste'>
                         <img className='w-20 m-auto py-4' src={react} alt="react" />
                         <p className='my-4'>React</p>
+                    </div>
+                    <div className='bg-blue3/05 shadow-md shadow-bgwindow rounded-md hover:scale-110 duration-300 hover:shadow-celeste'>
+                        <img className='w-20 m-auto py-4' src={nextjs} alt="nextjs" />
+                        <p className='my-4'>Next.js</p>
                     </div>
                     <div className='bg-blue3/05 shadow-md shadow-bgwindow rounded-md hover:scale-110 duration-300 hover:shadow-celeste'>
                         <img className='w-20 m-auto py-4' src={javascript} alt="javascript" />
@@ -37,6 +43,10 @@ const Skills = () => {
                     <div className='bg-blue3/05 shadow-md shadow-bgwindow rounded-md hover:scale-110 duration-300 hover:shadow-celeste'>
                         <img className='w-20 m-auto py-4' src={github} alt="github" />
                         <p className='my-4'>GitHub</p>
+                    </div>
+                    <div className='bg-blue3/05 shadow-md shadow-bgwindow rounded-md hover:scale-110 duration-300 hover:shadow-celeste'>
+                        <img className='w-20 m-auto py-4' src={tailwdindcss} alt="taiwindcss" />
+                        <p className='my-4'>Tailwindcss</p>
                     </div>
                 </div>
             </div>
--- src/components/works.tsx
+++ src/components/works.tsx
@@ -2,11 +2,13 @@ import work1 from '../assets/GreatCrusade.png'
 import work2 from '../assets/Wear.png'
 import work3 from '../assets/Pagura.png'
 import work4 from '../assets/Code.png'
+import work5 from '../assets/NextjsBlog.png'
+import work6 from '../assets/Socketio.png'
 
 const Works = () => {
     return (
         <div id='works' className='w-full md:h-screen snap-center'>
-            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='max-w-screen-2xl mx-auto p-4 flex flex-col justify-center w-full h-full'>
 
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-celeste'>Work</p>
@@ -14,7 +16,7 @@ const Works = () => {
                 </div>
 
                 {/*Container*/}
-                <div className='grid sm:grid-cols-2  gap-12'>
+                <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-20'>
 
                     { /*Item 1*/}
                     <div style={{ backgroundImage: `url(${work1})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div text-center'>
@@ -33,11 +35,28 @@ const Works = () => {
                         </div>
                     </div>
 
-                    {/*Item 2*/}
+                    { /*Item 2*/}
+                    <div style={{ backgroundImage: `url(${work5})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div text-center'>
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold -tracking-wider '>
+                                Next.js Blog using GraphQL, GraphCMS and Tailwindcss
+                            </span>
+                            <div className='text-center pt-8'>
+                                <a href="https://coffeeblog-nextjs-graphcms-tailwindcss.vercel.app/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href="https://github.com/BianchiLucas/Blog-NextJs-Graphql-Tailwindcss">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Item 3*/}
                     <div style={{ backgroundImage: `url(${work2})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div'>
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold -tracking-wider'>
-                                Interactive weather web app 
+                                Interactive weather web app
                             </span>
                             <div className='text-center pt-8'>
                                 <a href="https://whattowear.netlify.app/">
@@ -50,7 +69,7 @@ const Works = () => {
                         </div>
                     </div>
 
-                    { /*Item 3*/}
+                    { /*Item 4*/}
                     <div style={{ backgroundImage: `url(${work3})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div'>
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold -tracking-wider'>
@@ -60,14 +79,11 @@ const Works = () => {
                                 <a href="https://pagura.com.ar/">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Demo</button>
                                 </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Code</button>
-                                </a>
                             </div>
                         </div>
                     </div>
 
-                    { /*Item 4*/}
+                    { /*Item 5*/}
                     <div style={{ backgroundImage: `url(${work4})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div'>
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold -tracking-wider'>
@@ -79,6 +95,23 @@ const Works = () => {
                                 </a>
                                 <a href="https://github.com/BianchiLucas/Portfolio">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    { /*Item 6*/}
+                    <div style={{ backgroundImage: `url(${work6})` }} className='shadow-lg shadow-bgwindow group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold -tracking-wider'>
+                                Socket io React chat app whit Express
+                            </span>
+                            <div className='text-center pt-8'>
+                                <a href="https://github.com/BianchiLucas/socketio-react-project">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Frontend code</button>
+                                </a>
+                                <a href="https://github.com/BianchiLucas/socketio-project-server">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-bgwindow font-bold text-lg'>Server code</button>
                                 </a>
                             </div>
                         </div>
